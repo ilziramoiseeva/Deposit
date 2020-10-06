747c7e1f709d4a3fcaadeb80cc0064d285a1dbd7
ilziramoiseeva <ilzirkam@gmail.com>
2020-10-06 21:09:55 +0300

    Main.java added

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int percent = 2;
+        int sum = 23500_00;
+        int deposit = sum * percent / 100;
+        System.out.println(deposit);
+    }
+}
